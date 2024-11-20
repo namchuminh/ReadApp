c9b6f1d9774b0a47acb79508d8f45ca127bb8b57
Nam Chu Minh <chuminhnamma@gmail.com>
2024-11-20 19:17:29 +0700

    hoan thanh them sach vao thu vien

--- screens/DocSach/DocSach.js
+++ screens/DocSach/DocSach.js
@@ -2,15 +2,50 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { Appbar } from 'react-native-paper';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import CryptoJS from 'crypto-js';
 
-const DocSach = ({ navigation }) => {
+const decodeJWT = (token) => {
+  const parts = token.split('.');
+  if (parts.length !== 3) {
+      throw new Error('JWT không hợp lệ');
+  }
+  
+  const payload = parts[1];
+  const decoded = CryptoJS.enc.Base64.parse(payload);
+  return JSON.parse(decoded.toString(CryptoJS.enc.Utf8));
+};
+
+const DocSach = ({ route, navigation }) => {
   const [pdfUri, setPdfUri] = useState('http://10.0.2.2:8000/uploads/book/pdf/1731683046775.pdf'); // URL mặc định của PDF
+  const { id } = route.params;
 
   // Khi component mount, kiểm tra trạng thái đã lưu
   useEffect(() => {
-
+    console.log(id)
   }, []);
 
+  const handleBookMark = async () => {
+    const token = await AsyncStorage.getItem('token');
+    const decodedToken = decodeJWT(token);
+    try {
+        // Gửi yêu cầu GET đến API
+        const response = await axios.post(`http://10.0.2.2:8000/api/library`, {
+          MaNguoiDung: decodedToken.MaNguoiDung,
+          MaSach: id
+        });
+
+        alert(response.data.message)
+
+    } catch (error) {
+        if (error.response) {
+            alert(error.response.data.message);
+        } else {
+            alert("Không thể kết nối đến máy chủ. Vui lòng thử lại.");
+        }
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -20,7 +55,7 @@ const DocSach = ({ navigation }) => {
         <Appbar.Content title="Sách Mẫu - Tập 1" titleStyle={styles.headerTitle} />
         <Appbar.Action
           icon="bookmark-outline"
-          onPress={() => alert('Chức năng đánh dấu trang sẽ thêm sau.')}
+          onPress={() => handleBookMark()}
           color="#FFFFFF"
         />
       </Appbar.Header>
